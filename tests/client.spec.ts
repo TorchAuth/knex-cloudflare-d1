553fe3230e643177dff255c72f88fb30314c3f9f
Bruce Harrison <leeharrison1984@gmail.com>
2024-07-18 22:55:14 -0500

    fix file errors

--- tests/client.spec.ts
+++ tests/client.spec.ts
@@ -7,26 +7,18 @@ import { describe, it, expect } from "vitest";
 const bindings = getMiniflareBindings();
 
 describe("better-sqlite3 unit tests", () => {
-  describe("nativeBinding", () => {
-    it("should work with a custom `nativeBinding`", async () => {
-      const knexInstance = knex({
-        client: Client_D1 as unknown as typeof knex.Client,
-        useNullAsDefault: true,
-        connection: {
-          database: bindings.__D1_BETA__D1DATA,
-          options: {
-            nativeBinding: path.resolve(
-              __dirname,
-              "../node_modules/better-sqlite3/build/Release/better_sqlite3.node"
-            ),
-          },
-        },
-      });
-
-      const result = await knexInstance
-        .select(knexInstance.raw("2 + 2 as answer"))
-        .first();
-      expect(result.answer).to.equal(4);
+  it("basic query works", async () => {
+    const knexInstance = knex({
+      client: Client_D1 as unknown as typeof knex.Client,
+      useNullAsDefault: true,
+      connection: {
+        database: bindings.__D1_BETA__D1DATA,
+      },
     });
+
+    const result = await knexInstance
+      .select<{ answer: number }>(knexInstance.raw("2 + 2 as answer"))
+      .first();
+    expect(result.answer).to.equal(4);
   });
 });
